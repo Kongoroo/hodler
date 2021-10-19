de43ac3881418893ef7d9c94a78bed6d6e06f5f8
Malte Surmann <malte.surmann@xing.com>
2021-10-19 23:09:19 +0200

    chore: fix styles for the Elrond DApp

--- club/src/elrond/ping-pong/dapp-template/src/App.tsx
+++ club/src/elrond/ping-pong/dapp-template/src/App.tsx
@@ -10,62 +10,55 @@ import styled from "styled-components";
 
 export default function App() {
   return (
-    <StyledWrapper>
-      <Dapp.Context config={config}>
-        <ContextProvider>
-          <Layout>
-            <Switch>
-              <Route
-                path={routeNames.unlock}
-                component={() => (
-                  <Dapp.Pages.Unlock
-                    callbackRoute={routeNames.dashboard}
-                    title={config.dAppName}
-                    lead='Please select your login method:'
-                    ledgerRoute={routeNames.ledger}
-                    walletConnectRoute={routeNames.walletconnect}
-                  />
-                )}
-                exact={true}
-              />
-              <Route
-                path={routeNames.ledger}
-                component={() => (
-                  <Dapp.Pages.Ledger callbackRoute={routeNames.dashboard} />
-                )}
-                exact={true}
-              />
-              <Route
-                path={routeNames.walletconnect}
-                component={() => (
-                  <Dapp.Pages.WalletConnect
-                    callbackRoute={routeNames.dashboard}
-                    logoutRoute={routeNames.home}
-                    title='Maiar Login'
-                    lead='Scan the QR code using Maiar'
-                  />
-                )}
-                exact={true}
-              />
-
-              {routes.map((route, i) => (
-                <Route
-                  path={route.path}
-                  key={route.path + i}
-                  component={route.component}
-                  exact={true}
+    <Dapp.Context config={config}>
+      <ContextProvider>
+        <Layout>
+          <Switch>
+            <Route
+              path={routeNames.unlock}
+              component={() => (
+                <Dapp.Pages.Unlock
+                  callbackRoute={routeNames.dashboard}
+                  title={config.dAppName}
+                  lead='Please select your login method:'
+                  ledgerRoute={routeNames.ledger}
+                  walletConnectRoute={routeNames.walletconnect}
                 />
-              ))}
-              <Route component={PageNotFoud} />
-            </Switch>
-          </Layout>
-        </ContextProvider>
-      </Dapp.Context>
-    </StyledWrapper>
+              )}
+              exact={true}
+            />
+            <Route
+              path={routeNames.ledger}
+              component={() => (
+                <Dapp.Pages.Ledger callbackRoute={routeNames.dashboard} />
+              )}
+              exact={true}
+            />
+            <Route
+              path={routeNames.walletconnect}
+              component={() => (
+                <Dapp.Pages.WalletConnect
+                  callbackRoute={routeNames.dashboard}
+                  logoutRoute={routeNames.home}
+                  title='Maiar Login'
+                  lead='Scan the QR code using Maiar'
+                />
+              )}
+              exact={true}
+            />
+
+            {routes.map((route, i) => (
+              <Route
+                path={route.path}
+                key={route.path + i}
+                component={route.component}
+                exact={true}
+              />
+            ))}
+            <Route component={PageNotFoud} />
+          </Switch>
+        </Layout>
+      </ContextProvider>
+    </Dapp.Context>
   );
 }
-
-const StyledWrapper = styled.div`
-  margin: 0 auto;
-  width: 900px;
-`;
--- club/src/elrond/ping-pong/dapp-template/src/index.tsx
+++ club/src/elrond/ping-pong/dapp-template/src/index.tsx
@@ -1,15 +1,6 @@
 import * as React from "react";
-
 import App from "./App";
-import "../src/assets/sass/theme.scss";
 
 export default function Main() {
   return <App />;
 }
-
-// ReactDOM.render(
-//   <Router>
-//     <App />
-//   </Router>,
-//   document.getElementById("root"),
-// );
