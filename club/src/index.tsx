cd7dc613c432b07919d4d4cdb40b15ef2766675c
Malte Surmann <malte.surmann@xing.com>
2021-10-21 14:44:20 +0200

    chore: move scss file

--- club/src/index.tsx
+++ club/src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter as Router } from "react-router-dom";
 /* Elrond DApp styles */
-import "./assets/theme.scss";
+import "./theme.scss";
 
 function Main() {
   return (
