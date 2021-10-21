728131aa0d6a51c5c231d3c1ffce0ff7bae8c500
Malte Surmann <malte.surmann@xing.com>
2021-10-21 08:52:47 +0200

    chore: refactor index file

--- club/src/index.tsx
+++ club/src/index.tsx
@@ -7,16 +7,19 @@ import { BrowserRouter as Router } from "react-router-dom";
 /* Elrond DApp styles */
 import "../src/assets/theme.scss";
 
-ReactDOM.render(
-  <Router>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Router>,
-  document.getElementById("root")
-);
+function Main() {
+  return (
+    <Router>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Router>
+  );
+}
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+export default ReactDOM.render(<Main />, document.getElementById("root"));
